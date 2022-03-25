a7d0fe4aa5990026203c6e97d457827c6463f7c3
GiandomenicoIameo <g.iameo@studenti.unina.it>
2022-03-25 14:29:21 +0100

    add

--- Gioco delle monete/app/stack.js
+++ Gioco delle monete/app/stack.js
@@ -1,12 +1,12 @@
 function make() {
 
   stack = [
-    " 1" + '\u00A2',
-    " 2" + '\u00A2',
-    " 5" + '\u00A2',
-   " 10" + '\u00A2',
-   " 20" + '\u00A2',
-   " 50" + '\u00A2',
+    " 1"  + '\u00A2',
+    " 2"  + '\u00A2',
+    " 5"  + '\u00A2',
+    " 10" + '\u00A2',
+    " 20" + '\u00A2',
+    " 50" + '\u00A2',
     " 1€"
   ];
 
@@ -16,7 +16,7 @@ function make() {
 function member( stack ) {
 
   let string = stack.toString();
-  console.log( "\n{" + string + " }" );
+  console.log( "\nPortafogli = [" + string + " ]\n" );
 }
 
 module.exports = { make, member };
